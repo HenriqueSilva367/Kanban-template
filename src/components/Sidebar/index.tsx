95b19e514ff051133d44e18ea409565123b801f9
HenriqueSilva367 <jhenrique367@gmail.com>
2023-04-04 23:10:00 -0300

    criando componentes separados

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,39 @@
+import Logo from '../../assets/Logo.svg';
+import './style.css'
+
 function SideBar() {
   return (
     <div className="container">
-      
+      <nav>
+        <img src={Logo} alt="Logo do site" className="logo" />
+
+        <ul>
+          <li>
+            <a href="#" className="active">
+              
+              Boards
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              
+              Equipes
+            </a>
+          </li>
+          <li>
+            <a href="#">
+             
+              Relatórios
+            </a>
+          </li>
+          <li>
+            <a href="#">
+             
+              Ajustes
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,94 @@
 import './style.css'
+import Pencil from '../assets/pencil.svg';
+
+import SideBar from '../components/Sidebar';
+
 function App() {
   return (
-    <div className="App">
-      <div className='main'>
-    
-      </div>
+    <div id="app">
+      <SideBar/>
+      <main>
+        <div className="inner">
+          <section className="title-avatar">
+            <h1>Meu KanBan <img src={Pencil} alt="" /></h1>
+            <img src="https://github.com/HenriqueSilva367.png" alt="avatar" />
+          </section>
+
+          <section className="filter">
+            <button>
+              
+              <span>Filtrar</span>
+            </button>
+            <div className="input-wrapper">
+              
+              <input
+                type="text"
+                placeholder="Busque por cards, assuntos ou responsáveis..."
+              />
+            </div>
+          </section>
+          <section className="kanban">
+            <div className="todo">
+              <h2>A fazer</h2>
+              <div className="cards">
+                <div className="card">
+                  <h3>#boraCodar um Kanban 🧑🏾‍💻</h3>
+                  <p>
+                    Novo desafio do #boraCodar da Rocketseat, onde é proposto
+                    construir um quadro de Kanban.
+                  </p>
+                  <div className="tags">
+                    <span>rocketseat</span>
+                    <span>desafio</span>
+                  </div>
+                </div>
+                <div className="card">
+                  <h3>Manter a ofensiva 🔥</h3>
+                  <p>
+                    Manter minha atividade na plataforma da Rocketseat para não
+                    perder a ofensiva
+                  </p>
+                  <div className="tags">
+                    <span>rocketseat</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="doing">
+              <h2>Fazendo</h2>
+              <div className="cards">
+                <div className="card">
+                  <h3>Conferir o novo desafio 🚀</h3>
+                  <p>
+                    Conferir o novo projeto do #boraCodar para fazê-lo da melhor
+                    maneira possível
+                  </p>
+                  <div className="tags">
+                    <span>rocketseat</span>
+                    <span>desafio</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="done">
+              <h2>Feito</h2>
+              <div className="cards">
+                <div className="card">
+                  <h3>#boraCodar uma página de login 🧑‍💻</h3>
+                  <p>
+                    Novo desafio do #boraCodar da Rocketseat, onde é proposto
+                    construir uma página de login.
+                  </p>
+                  <div className="tags">
+                    <span>rocketseat</span>
+                    <span>desafio</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   )
 }
